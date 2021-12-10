03f0269b2d833597073a7798de48be9bfe962d4d
jasonbdv <jasonbenedict.verde.iics@ust.edu.ph>
2021-12-10 15:29:26 +0000

    All three features added

--- Project_Activity5.py
+++ Project_Activity5.py
@@ -1,73 +1,54 @@
 from ncclient import manager
 import xml.dom.minidom
 m = manager.connect(
- host="192.168.56.101",
+ host="192.168.56.107",
  port=830,
  username="cisco",
  password="cisco123!",
  hostkey_verify=False
  )
-print("#Supported Capabilities (YANG models):")
-for capability in m.server_capabilities:
- print(capability)
- netconf_reply = m.get_config(source="running")
-print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
-netconf_filter = """
-<filter>
- <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native" />
-</filter>
-"""
-netconf_reply = m.get_config(source="running", filter=netconf_filter)
-print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
-netconf_hostname = """
+
+netconf_password_encryption = """
 <config>
  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
- <hostname>CSR1kv</hostname>
+    <service>
+        <password-encryption/>
+    </service>
  </native>
 </config>
 """
-netconf_reply = m.edit_config(target="running", config=netconf_hostname)
-print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
-netconf_loopback = """
+netconf_reply1 = m.edit_config(target="running", config=netconf_password_encryption)
+print(xml.dom.minidom.parseString(netconf_reply1.xml).toprettyxml())
+
+line_con = """
 <config>
-<native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
- <interface>
- <Loopback>
- <name>1</name>
- <description>My NETCONF loopback</description>
- <ip>
- <address>
- <primary>
- <address>10.1.1.1</address>
- <mask>255.255.255.0</mask>
- </primary>
- </address>
- </ip>
- </Loopback>
- </interface>
-</native>
+ <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+    <line>
+        <console>
+            <first>0</first>
+            <login/>
+            <password>
+                <secret>admin</secret>
+            </password>
+            
+        </console>
+    </line>
+ </native>
 </config>
 """
-netconf_reply = m.edit_config(target="running", config=netconf_loopback)
-print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
-netconf_newloop = """
+netconf_reply2 = m.edit_config(target="running", config=line_con)
+print(xml.dom.minidom.parseString(netconf_reply2.xml).toprettyxml())
+
+password = """
 <config>
-<native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
- <interface>
- <Loopback>
- <name>2</name>
- <description>My second NETCONF loopback</description>
- <ip>
- <address>
- <primary>
- <address>10.1.1.1</address>
- <mask>255.255.255.0</mask>
- </primary>
- </address>
- </ip>
- </Loopback>
- </interface>
-</native>
+ <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+    <enable>
+        <password>
+            <secret>test</secret>
+        </password>
+    </enable>
+ </native>
 </config>
 """
-netconf_reply = m.edit_config(target="running", config=netconf_newloop)
+netconf_reply3 = m.edit_config(target="running", config=password)
+print(xml.dom.minidom.parseString(netconf_reply3.xml).toprettyxml())
